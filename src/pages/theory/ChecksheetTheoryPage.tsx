37a43363cd7b10c66a9cc0b95e1e5958cef93e68
diazadriansyah <diazadr.dev@gmail.com>
2025-12-05 11:59:19 +0700

    update

--- src/pages/theory/ChecksheetTheoryPage.tsx
+++ src/pages/theory/ChecksheetTheoryPage.tsx
@@ -45,6 +45,8 @@ const ChecksheetTheoryPage = () => {
               <li>{t("checksheet_theory.A_imp3")}</li>
               <li>{t("checksheet_theory.A_imp4")}</li>
             </ul>
+
+           <img src="/img/defective.png" className="max-w-[240px] mx-auto rounded-md" />
           </div>
 
           <div className="space-y-3">
@@ -59,6 +61,8 @@ const ChecksheetTheoryPage = () => {
               <li>{t("checksheet_theory.B_imp3")}</li>
               <li>{t("checksheet_theory.B_imp4")}</li>
             </ul>
+
+            <img src="/img/location.png" className="max-w-[240px] mx-auto rounded-md" />
           </div>
 
           <div className="space-y-3">
@@ -73,6 +77,8 @@ const ChecksheetTheoryPage = () => {
               <li>{t("checksheet_theory.C_imp3")}</li>
               <li>{t("checksheet_theory.C_imp4")}</li>
             </ul>
+
+            <img src="/img/cause.png" className="max-w-[240px] mx-auto rounded-md" />
           </div>
 
           <div className="space-y-3">
@@ -87,37 +93,9 @@ const ChecksheetTheoryPage = () => {
               <li>{t("checksheet_theory.D_imp3")}</li>
               <li>{t("checksheet_theory.D_imp4")}</li>
             </ul>
+
+           <img src="/img/distribution.png" className="max-w-[240px] mx-auto rounded-md" />
           </div>
-        </motion.section>
-
-        <motion.section variants={itemFade} className="space-y-4 mt-14">
-          <h2 className="text-2xl font-bold">{t("checksheet_theory.section3_title")}</h2>
-
-          <h4 className="font-semibold">{t("checksheet_theory.section3_p1_title")}</h4>
-          <p>{t("checksheet_theory.section3_p1_text")}</p>
-
-          <h4 className="font-semibold">{t("checksheet_theory.section3_p2_title")}</h4>
-          <p>{t("checksheet_theory.section3_p2_text")}</p>
-
-          <h4 className="font-semibold">{t("checksheet_theory.section3_p3_title")}</h4>
-          <p>{t("checksheet_theory.section3_p3_text")}</p>
-
-          <h4 className="font-semibold">{t("checksheet_theory.section3_p4_title")}</h4>
-          <p>{t("checksheet_theory.section3_p4_text")}</p>
-        </motion.section>
-
-        <motion.section variants={itemFade} className="space-y-4 mt-14">
-          <h2 className="text-2xl font-bold">{t("checksheet_theory.section4_title")}</h2>
-
-          <p>{t("checksheet_theory.section4_point1")}</p>
-          <p>{t("checksheet_theory.section4_point2")}</p>
-          <p>{t("checksheet_theory.section4_point3")}</p>
-          <p>{t("checksheet_theory.section4_point4")}</p>
-        </motion.section>
-
-        <motion.section variants={itemFade} className="space-y-4 mt-14">
-          <h2 className="text-2xl font-bold">{t("checksheet_theory.section5_title")}</h2>
-          <p>{t("checksheet_theory.section5_text")}</p>
         </motion.section>
 
       </MotionSection>
